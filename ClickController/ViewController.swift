4fa1c026011ef43e3bc016b44b2236d588f1bcda
abbasaniefa <abbasvlb@gmail.com>
2016-02-25 09:04:49 +0530

    Added buttons to increment and decrement values

--- ClickController/ViewController.swift
+++ ClickController/ViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var label:UILabel!
+    var count:Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        label=UILabel()
+        label.frame=CGRectMake(150, 150, 60, 60)
+        label.text = "0"
+        
+        self.view.addSubview(label)
+        
+        var incrementButton=UIButton()
+        incrementButton.frame=CGRectMake(100, 250, 60, 60)
+        incrementButton.setTitle("Inc", forState: .Normal)
+        incrementButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        
+        self.view.addSubview(incrementButton)
+        
+        var decrementButton=UIButton()
+        decrementButton.frame=CGRectMake(170, 250, 60, 60)
+        decrementButton.setTitle("Dec", forState: .Normal)
+        decrementButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        
+        self.view.addSubview(decrementButton)
+        
+        incrementButton.addTarget(self, action: "incrementMethod", forControlEvents: UIControlEvents.TouchUpInside)
+        decrementButton.addTarget(self, action: "decrementMethod", forControlEvents: UIControlEvents.TouchUpInside)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +46,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func incrementMethod(){
+        self.count++
+        self.label.text = "\(self.count)"
+    }
+    
+    func decrementMethod(){
+        self.count--
+        self.label.text = "\(self.count)"
+    }
 
 }
 
