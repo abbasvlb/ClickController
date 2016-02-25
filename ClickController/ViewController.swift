c602479a433fe5e35532a0111042023e6b633093
abbasaniefa <abbasvlb@gmail.com>
2016-02-25 08:35:09 +0530

    Initial Commit

---
+++ ClickController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClickController
+//
+//  Created by Abbas Aniefa on 25/02/16.
+//  Copyright © 2016 Abbas Aniefa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
